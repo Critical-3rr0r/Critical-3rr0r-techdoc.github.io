29e023384e294ca0454e20b968d0f790a317afa8
Critical-3rr0r <dalton.robinson36@gmail.com>
2024-10-08 17:19:09 -0500

    Fixed white screen issue

--- projects/mrkdwn/vite.config.js
+++ projects/mrkdwn/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./critical-3rr0r.github.io/projects/Markdown",
+  base: "https://critical-3rr0r.github.io/projects/Markdown/",
   plugins: [react()],
 })
