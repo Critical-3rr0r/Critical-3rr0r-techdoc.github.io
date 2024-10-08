4b3b95da8cbd9835f5eccd41e49c663790749d96
Critical-3rr0r <dalton.robinson36@gmail.com>
2024-10-08 16:58:13 -0500

    added markdown page

---
+++ projects/mrkdwn/vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/critical-3rr0r.github.io/projects/Markdown",
+  plugins: [react()],
+})
